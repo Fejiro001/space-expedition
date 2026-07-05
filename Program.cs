a2e466c928ef7c8f77da4693856c87c9ba899c2c
Fejiro001 <aberefejiro@gmail.com>
2026-07-04 23:46:16 -0500

    Initialized inventory array, artifact count, and vault file variables. Added a startup message to indicate the system is initialized.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Artifact[] inventory = new Artifact[5];
+            int artifactCount = 0;
+            string vaultFile = "galactic_vault.txt";
+
+            Console.WriteLine("--- GALACTIC INVENTORY SYSTEM INITIALIZED ---");
         }
     }
 }
